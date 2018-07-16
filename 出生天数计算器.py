ac29a1daebf55d8e61b4fa6405b1fec2e66857ce
D3Change <40330410+D3Change@users.noreply.github.com>
2018-07-16 14:51:22 +0800

    Update 出生天数计算器.py

--- 出生天数计算器.py
+++ 出生天数计算器.py
@@ -1,3 +1,4 @@
+#Firstly,we should define a function which will point out the next day of the day we input.
 def nextday(year, month, day):
     if day < daysinmonth(year, month):
         return year, month, day + 1
@@ -7,7 +8,7 @@ def nextday(year, month, day):
         else:
             return year, month + 1, 1
 
-
+#Then,we must confirm the date order is the right order.
 def dateisbefore(year1, month1, day1, year2, month2, day2):
     if year1 < year2:
         return True
@@ -18,7 +19,7 @@ def dateisbefore(year1, month1, day1, year2, month2, day2):
             return day1 < day2
     return False
 
-
+#After that, we should make a list which can return the day in month.
 def daysinmonth(year, month):
     if month in [4, 6, 9, 11]:
         return 30
@@ -28,7 +29,7 @@ def daysinmonth(year, month):
         return 28
     return 31
 
-
+#Besides,we must know if the year you input is leap year or not.
 def isleapyear(year):
     if year % 400 == 0:
         return True
@@ -38,7 +39,7 @@ def isleapyear(year):
         return True
     return False
 
-
+#In the end, we can define the main function of the calculator.
 def daysbetweendates(year1, month1, day1, year2, month2, day2):
     assert not dateisbefore(year2, month2, day2, year1, month1, day1)
     days = 0
@@ -48,19 +49,7 @@ def daysbetweendates(year1, month1, day1, year2, month2, day2):
     return days
 
 
-def test():
-    test_cases = [((2012, 1, 1, 2012, 2, 28), 58),
-                  ((2012, 1, 1, 2012, 3, 1), 60),
-                  ((2011, 6, 30, 2012, 6, 30), 366),
-                  ((2011, 1, 1, 2012, 8, 8), 585),
-                  ((1900, 1, 1, 1999, 12, 31), 36523)]
 
-    for (args, answer) in test_cases:
-        result = daysbetweendates(*args)
-        if result != answer:
-            print("Test with data:", args, "failed")
-        else:
-            print("Test case passed!")
-
-
-test()
+if __name__ == '__main__':
+    daysbetweendates(year1, month1, day1, year2, month2, day2)
+#REMBER:please input the first date and the second date in order.
